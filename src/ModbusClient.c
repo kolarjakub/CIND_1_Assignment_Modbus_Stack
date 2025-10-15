bd1f4af48a6c44411c54a7988f3d80fd52f31f89
kuko <jakub03kola@gmail.com>
2025-10-15 22:37:08 +0000

    testing in main

--- src/ModbusClient.c
+++ src/ModbusClient.c
@@ -67,8 +67,9 @@ int main(){
     while (1) {
         for (int i=1;i<=65536;i=i+2) {
             Read_h_regs_print(server_add, SERVER_PORT, i, number_of_regs, read_buf);
+            usleep(100000);  // 100 000 mikrosekund = 0.1 s
             Write_multiple_regs_print(server_add, SERVER_PORT, i, number_of_regs, write_buf);
-            usleep(1000000);  // 100 000 mikrosekund = 0.1 s
+            usleep(1000000);  // 1000 000 mikrosekund = 1 s
             printf("\n");
         }
     }
